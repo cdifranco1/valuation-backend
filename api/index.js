3113a2526e2117e9ad5710921a45bd20ddbe78e7
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-06-08 23:32:52 -0500

    setting up to push to heroku

--- api/index.js
+++ api/index.js
@@ -1,16 +1,17 @@
 // set up database connection and routing here
 const mongoose = require('mongoose')
 const dcfRoute = require('./routes/dcf')
-const registerRoute = require('./routes/register')
-const loginRoute = require('./routes/login')
 const router = require('express').Router()
+
+require('dotenv').config()
 
 mongoose.set('useFindAndModify', false )
 mongoose.set('useNewUrlParser', true )
 mongoose.set('useUnifiedTopology', true )
 mongoose.set('useCreateIndex', true )
 
-mongoose.connect(`mongodb://localhost/valuation`)
+mongoose.connect(process.env.MONGODB_URI ||
+  `mongodb://localhost/valuation`)
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err))
 
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const apiRoute = require('./api')
+require('dotenv').config()
 
 const port = process.env.PORT || 5000
 
